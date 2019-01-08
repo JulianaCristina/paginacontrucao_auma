4907f7243194de5ba28c47796d73376b73be48f6
Juliana-PC\Juliana <julianacristina780@gmail.com>
2019-01-08 09:38:09 -0200

    Página em Construção Auma

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Auma Negócios</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="author" href="https://dvos.com.br/" />
+	<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" type="image/png" href="images/favicon.png"/>	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row total">
+			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 escrita">
+				<img src="images/escrita_1.png" alt="">
+			</div>
+		</div>
+			<div class=" contato">
+				<span class="telefone">
+					<a href="tel:+055(34)3826-1100">
+						(34) 3826-1100
+					</a>
+				</span>
+				<span class="separador">
+						|  
+				</span>
+				<span class="email">
+					<a href="mailto:contato@auma.com.br">
+						contato@auma.com.br
+					</a>
+				</span>
+			</div>
+			<div class="social-media">
+				<a href="https://www.instagram.com/aumanegocios/" title="">
+					<img src="images/insta_1.png" alt="">					
+				</a>
+				<a href="https://www.facebook.com/AumaNegocios/" title="">
+					<img src="images/face_1.png" alt="">
+				</a>
+			</div>
+		
+	</div>
+</body>
+</html>
